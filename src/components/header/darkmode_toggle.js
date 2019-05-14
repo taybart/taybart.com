a985d61a4fea23d78fe68942f80f433e7ac95870
Taylor <taybart@gmail.com>
2019-05-14 13:34:46 -0600

    Add much better autodetect for dark mode

--- src/components/header/darkmode_toggle.js
+++ src/components/header/darkmode_toggle.js
@@ -3,6 +3,9 @@ import style from './style.module.css';
 import sun from '../../assets/sun.png';
 import moon from '../../assets/moon.png';
 
+const DARK = '(prefers-color-scheme: dark)';
+const LIGHT = '(prefers-color-scheme: light)';
+
 export default class DarkmodeToggle extends Component {
   constructor(props) {
     super(props);
@@ -12,29 +15,55 @@ export default class DarkmodeToggle extends Component {
   }
 
   componentDidMount() {
-    if ((localStorage.getItem('mode') || 'light') === 'light') {
-      this.setlight()
-    } else {
-      this.setdark()
+    this.addColorSchemeListeners();
+    const mode = localStorage.getItem('mode');
+    if (mode === 'light') {
+      this.setLight()
+    } else if (mode === 'dark') {
+      this.setDark()
+    } else if (!this.sysColorScheme()) {
+      // if no previous mode, attempt to autodetect
+      this.setlight(); // default to light
+    }
+  }
+
+  addColorSchemeListeners = () => {
+    if(!window.matchMedia) { return }
+    window.matchMedia(DARK).addListener((e) => this.checkSysScheme(e));
+    window.matchMedia(LIGHT).addListener((e) => this.checkSysScheme(e));
+  }
+  sysColorScheme = () => {
+    return this.checkSysScheme(window.matchMedia(DARK)) || this.checkSysScheme(window.matchMedia(LIGHT));
+  }
+  checkSysScheme = ({matches, media}) => {
+    if(!matches) {
+      return false;
+    } else if(media === DARK) {
+      this.setDark()
+      return true;
+    } else if (media === LIGHT) {
+      this.setLight()
+      return true;
     }
   }
 
   toggleMode = () => {
-    console.log(localStorage.getItem('mode'));
     if (localStorage.getItem('mode') === 'light') {
-      this.setdark();
+      this.setDark();
     } else {
-      this.setlight();
+      this.setLight();
     }
   }
 
-  setdark = () => {
+  setmode = (mode) => {
+  }
+  setDark = () => {
     localStorage.setItem('mode', 'dark');
     document.querySelector('body').classList.add('dark');
     document.querySelector('header').classList.add('dark');
     this.setState({ img: sun });
   }
-  setlight = () => {
+  setLight = () => {
     localStorage.setItem('mode', 'light');
     document.querySelector('body').classList.remove('dark');
     document.querySelector('header').classList.remove('dark');
