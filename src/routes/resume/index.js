c2f6ccfdbf107b0b9ffa334a75e39f377d8eeb2c
Taylor <taybart@gmail.com>
2019-10-06 11:38:09 -0600

    Update resume

--- src/routes/resume/index.js
+++ src/routes/resume/index.js
@@ -3,60 +3,57 @@ import { Link } from 'react-router-dom';
 import style from './style.module.css';
 
 const Resume = () => (<div className={style.resume}>
-  <b>things i know well</b>
-  <ul>
-    <li> language </li>
-    <ul>
-      <li> go </li>
-      <li> js/node </li>
-      <li> python </li>
-      <li> c </li>
+  <div className={style.resumelist}>
+    <ul className={style.list}>
+      <li>2015 bs electrical and computer engineering, university of colorado</li>
+      <li>2014-2017 <b>co-founder</b> mfactor engineering</li>
+      <li>2017-2018 <b>lead engineer</b> the spiga group</li>
+      <li>2018-now <b>lead engineer</b> journey</li>
     </ul>
-    <li> web frameworks </li>
+    <b>things i know well</b>
     <ul>
-      <li> react </li>
+      <li> language </li>
+      <ul>
+        <li> go </li>
+        <li> js/node </li>
+        <li> python </li>
+        <li> c </li>
+      </ul>
+      <li> web frameworks </li>
+      <ul>
+        <li> react </li>
+      </ul>
+      <li> ops </li>
+      <ul>
+        <li> linux </li>
+        <li> docker </li>
+        <li> aws...all the aws </li>
+      </ul>
     </ul>
-    <li> storage </li>
+    {/* <b>things i am learning</b>
     <ul>
-      <li> bolt </li>
-      <li> redis </li>
-      <li> sqlite </li>
-      <li> postgres </li>
-      <li> mongodb </li>
-    </ul>
-    <li> ops </li>
+      <li> language </li>
+      <ul> </ul>
+      <li> frameworks </li>
+      <ul> </ul>
+    </ul> */}
+    <b>things i made</b>
     <ul>
-      <li> docker </li>
-      <li> linux </li>
-      <li> aws...all the aws </li>
+      <li>
+        <a href="https://github.com/taybart/fm">fm</a>
+      </li>
+      <li>
+        <a href="https://github.com/taybart/log">log</a>
+      </li>
+      <li>
+        <a href="https://github.com/taybart/paint">paint</a>
+      </li>
+      <li>
+        <Link to="/hn">my hacker news</Link>
+      </li>
     </ul>
-  </ul>
-  <b>things i am learning</b>
-  <ul>
-    <li> language </li>
-    <ul>
-      <li> rust </li>
-      <li> CUDA </li>
-    </ul>
-    <li> frameworks </li>
-    <ul>
-    </ul>
-  </ul>
-  <b>things i made</b>
-  <ul>
-    <li>
-      <a href="https://github.com/taybart/fm">fm</a>
-    </li>
-    <li>
-      <a href="https://github.com/taybart/log">log</a>
-    </li>
-    <li>
-      <a href="https://github.com/taybart/paint">paint</a>
-    </li>
-    <li>
-      <Link to="/hn">my hacker news</Link>
-    </li>
-  </ul>
-</div>);
+  </div>
+</div>
+);
 
 export default Resume;
