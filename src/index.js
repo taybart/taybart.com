d3a3c8bd580315ff9126356cfefe4e382aa37d82
Taylor <taybart@gmail.com>
2019-11-24 19:08:25 -0700

    Remove app component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import './index.css';
-import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 
+import Header from './components/header';
+
+// Code-splitting is automated for routes
+// import Home from '../routes/home';
+import Resume from './routes/resume';
+import HN from './routes/hn';
+import VC from './routes/voicechat';
+import Login from './routes/login';
+
+function App () {
+    return (
+      <div id="app">
+        <Router>
+          <Header />
+          <Route path="/" exact>
+            <Redirect to="/resume" />
+          </Route>
+          <Route path="/resume" component={Resume} />
+          <Route path="/hn" component={HN} />
+          <Route path="/vc" component={VC} />
+          <Route path="/login" component={Login} />
+        </Router>
+      </div>
+    )
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
