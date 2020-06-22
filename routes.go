de7f6f3365ca073b91ddfd5401429e49655a9a39
Taylor <taybart@gmail.com>
2020-06-21 19:13:23 -0600

    Add logging middleware

--- routes.go
+++ routes.go
@@ -1,6 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
 func (s *server) routes() {
+	s.router.Use(gin.LoggerWithConfig(gin.LoggerConfig{
+		Formatter: func(param gin.LogFormatterParams) string {
+			var statusColor, methodColor, resetColor string
+			if param.IsOutputColor() {
+				statusColor = param.StatusCodeColor()
+				methodColor = param.MethodColor()
+				resetColor = param.ResetColor()
+			}
+
+			if param.Latency > time.Minute {
+				param.Latency.Truncate(time.Second)
+			}
+			return fmt.Sprintf("%v | %s %3d %s| %13v | %15s |%s %-7s %s %s\n%s",
+				param.TimeStamp.Format("2006-01-02T15:04:05"),
+				statusColor, param.StatusCode, resetColor,
+				param.Latency,
+				param.ClientIP,
+				methodColor, param.Method, resetColor,
+				param.Path,
+				param.ErrorMessage,
+			)
+		},
+		SkipPaths: []string{"/hc"},
+	}))
+
+	s.router.Use(gin.Recovery())
+	s.router.GET("/hc", func(c *gin.Context) { c.Status(http.StatusOK) })
+
 	s.router.LoadHTMLGlob("./templates/*/*")
 
 	s.router.Static("/img", "./static/img")
