c6039781f0bd51fd3fede1c914bfb7678b4df9fb
Taylor <taybart@gmail.com>
2021-06-29 16:07:56 -0600

    Add better online check

--- frontend/src/routes/notes/Edit.tsx
+++ frontend/src/routes/notes/Edit.tsx
@@ -1,17 +1,16 @@
-import React, {FC, useState, useEffect, useMemo} from 'react'
-import {BaseEditor, createEditor, Node} from 'slate'
-import {Slate, Editable, ReactEditor, withReact, } from 'slate-react'
+import React, { FC, useState, useEffect, useMemo } from "react";
+import { BaseEditor, createEditor, Node } from "slate";
+import { Slate, Editable, ReactEditor, withReact } from "slate-react";
 
 const serialize = (nodes: Node[]) => {
-  return nodes.map(n => Node.string(n)).join('\n')
-}
+  return nodes.map((n) => Node.string(n)).join("\n");
+};
 
+export type CustomEditor = BaseEditor & ReactEditor;
 
-export type CustomEditor = BaseEditor & ReactEditor
-
-declare module 'slate' {
+declare module "slate" {
   interface CustomTypes {
-    Editor: CustomEditor
+    Editor: CustomEditor;
   }
 }
 
@@ -22,29 +21,53 @@ interface Props {
 }
 
 const parse = (note: string): Node[] => {
-  return note.split('\n').map(s => ({type: 'paragraph', children: [{text: s}]}))
-}
+  return note
+    .split("\n")
+    .map((s) => ({ type: "paragraph", children: [{ text: s }] }));
+};
 
-const Edit: FC<Props> = ({note, online, onExit}) => {
-  const [value, setValue] = useState<Node[]>([])
-  const editor = useMemo(() => withReact(createEditor()), [])
+const Edit: FC<Props> = ({ note, online, onExit }) => {
+  const [value, setValue] = useState<Node[]>([]);
+  const editor = useMemo(() => withReact(createEditor()), []);
 
   useEffect(() => {
-    setValue(parse(note))
-  }, [])
+    setValue(parse(note));
+  }, []);
 
-
-  return (<div className="flex flex-col w-full items-left justify-center py-10">
-    <div className="back pl-10 cursor-pointer lg:pb-0 pb-14 underline" onClick={() => onExit(serialize(value))}>
-      finish
-      {!online && <div>offline</div>}
+  const onClick = () => {
+    if (!online) {
+      alert("not online, your work will not be saved");
+    }
+    onExit(serialize(value));
+  };
+  return (
+    <div className="flex flex-col w-full items-left justify-center py-10">
+      <div className="flex flex-row justify-between w-full px-10">
+        <div className="back cursor-pointer lg:pb-0 pb-14" onClick={onClick}>
+          finish
+        </div>
+        {!online && (
+          <div className="cursor-pointer lg:pb-0 pb-14 text-red-600">
+            offline!
+          </div>
+        )}
+        <div />
+      </div>
+      <div
+        className={`md:mx-56 mx-4 p-10 border ${
+          online ? "border-dark dark:border-white" : "border-red-600"
+        } h-full`}
+      >
+        <Slate
+          editor={editor}
+          value={value}
+          onChange={(update) => setValue(update)}
+        >
+          <Editable />
+        </Slate>
+      </div>
     </div>
-    <div className={`md:mx-56 mx-4 p-10 border ${online ? "border-dark dark:border-white" : "border-red-600"} h-full`}>
-      <Slate editor={editor} value={value} onChange={update => setValue(update)}>
-        <Editable />
-      </Slate>
-    </div >
-  </div >)
-}
+  );
+};
 
-export default Edit
+export default Edit;
--- frontend/src/routes/notes/Note.tsx
+++ frontend/src/routes/notes/Note.tsx
@@ -1,72 +1,91 @@
-import React, {FC, useState, useEffect} from 'react'
-import {Link, useHistory, useParams} from 'react-router-dom'
-import Markdown from 'markdown-to-jsx';
+import React, { FC, useState, useEffect } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
+import Markdown from "markdown-to-jsx";
 
-import Edit from './Edit'
-import EditIcon from './EditIcon'
-import Loading from '../../components/loading'
+import Edit from "./Edit";
+import EditIcon from "./EditIcon";
+import Loading from "../../components/loading";
 
-import {getNote, updateNote} from '../../util/api'
+import { getNote, updateNote } from "../../util/api";
 
 const Note: FC = () => {
-  const [ready, setReady] = useState<boolean>(false)
-  const [edit, setEdit] = useState<boolean>(false)
-  const [online, setOnline] = useState<boolean>(false)
-  const [note, setNote] = useState<{id: string, body: string}>({id: '', body: ''})
-  const history = useHistory()
-  const params = useParams<{id: string}>()
-
+  const [ready, setReady] = useState<boolean>(false);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [online, setOnline] = useState<boolean>(true);
+  const [to, setTO] = useState<number>(-1);
+  const [note, setNote] = useState<{ id: string; body: string }>({
+    id: "",
+    body: "",
+  });
+  const history = useHistory();
+  const params = useParams<{ id: string }>();
 
   useEffect(() => {
-    getNote(params.id).then(({note, msg, error}) => {
+    getNote(params.id).then(({ note, msg, error }) => {
       if (error) {
-        if (msg === 'unauthorized') {
-          history.push('/login')
+        if (msg === "unauthorized") {
+          history.push("/login");
         }
-        return
+        return;
       }
       if (note) {
-        setNote({id: params.id, body: note})
+        setNote({ id: params.id, body: note });
       }
-      setReady(true)
-    })
-  }, [])
-
-  useEffect(() => {
-    const handleStatus = () => setOnline(navigator.onLine)
-    window.addEventListener('offline', handleStatus)
-    window.addEventListener('online', handleStatus)
-
-    // cleanup if we unmount
-    return () => {
-      window.removeEventListener('offline', handleStatus)
-      window.removeEventListener('online', handleStatus)
-    }
+      setReady(true);
+    });
   }, []);
 
+  useEffect(() => {
+    if (edit) {
+      if (to) {
+        clearTimeout(to);
+      }
+      setTO(
+        setTimeout(() => {
+          fetch("/hc")
+            .then((res) => setOnline(res.status === 200))
+            .catch(() => setOnline(false));
+        }, 5000)
+      );
+    }
+    return () => {
+      clearTimeout(to);
+    };
+  }, [edit]);
+
   if (!ready) {
-    return <Loading className="m-auto pt-16" />
+    return <Loading className="m-auto pt-16" />;
   }
   if (edit) {
-    return <Edit note={note.body} online={online} onExit={(n: string) => {
-      setNote({id: params.id, body: n})
-      setEdit(false)
-      if (note.body !== n) {
-        updateNote(params.id, n)
-      }
-    }} />
+    return (
+      <Edit
+        note={note.body}
+        online={online}
+        onExit={(n: string) => {
+          setNote({ id: params.id, body: n });
+          setEdit(false);
+          if (note.body !== n) {
+            updateNote(params.id, n);
+          }
+        }}
+      />
+    );
   }
 
-  return (<div className="w-full pt-10">
-    <div className="flex flex-row w-full justify-between pb-10 px-10">
-      <Link className="back" to="/notes">back</Link>
-      <EditIcon className="cursor-pointer stroke-1" onClick={() => setEdit(true)} />
+  return (
+    <div className="w-full pt-10">
+      <div className="flex flex-row w-full justify-between pb-10 px-10">
+        <Link className="back" to="/notes">
+          back
+        </Link>
+        <EditIcon
+          className="cursor-pointer stroke-1"
+          onClick={() => setEdit(true)}
+        />
+      </div>
+      <Markdown className="markdown">{note.body}</Markdown>
     </div>
-    <Markdown className="markdown">
-      {note.body}
-    </Markdown>
-  </div>)
-}
+  );
+};
 
-
-export default Note
+export default Note;
