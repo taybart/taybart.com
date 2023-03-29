c00934060564ae831d14cddbaf92ffe512d77cf4
Taylor Bartlett <taybart@gmail.com>
2023-03-28 19:37:25 -0600

    chore: remove weird files

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,16 +1,20 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import solid from '@astrojs/solid-js'
+import solid from '@astrojs/solid-js';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
   output: 'server',
   server: {
-    port: 9999,
+    port: 9999
   },
-})
+  adapter: vercel()
+});
